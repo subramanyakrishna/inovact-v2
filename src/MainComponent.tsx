03c9ef928022c8c1120c0520a356bfcc45869155
PanavSethi <panav.sethi.delhi@gmail.com>
2021-08-08 20:19:02 +0530

    chore: added sass folder structure and routing

---
+++ src/MainComponent.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Signup from './components/Signup'
+import SignupForm from './components/SignupForm'
+
+function MainComponent() {
+    return (
+        <div className="Main">
+            <Router>
+                <Switch>
+                    <Route path="/signup" component={Signup} />
+                    <Route path="/form" component={SignupForm} />
+                </Switch>
+            </Router>
+        </div>
+    )
+}
+
+export default MainComponent
