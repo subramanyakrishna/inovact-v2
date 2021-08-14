13220a3d5273ae725192b82fc34b2e1272e27f58
Nidhi Hemanth <nidhihemanth123@gmail.com>
2021-08-14 22:07:15 +0530

    build: signup done, needs tweaks in responsiveness below 370px

--- src/MainComponent.tsx
+++ src/MainComponent.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Signup from './components/Signup'
+import Signup from './components/signup/Signup'
 import SignupForm from './components/SignupForm'
 
 function MainComponent() {
