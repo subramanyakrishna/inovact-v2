0c105415c1c2aad9d471dc2ab87a84ad2d401008
PanavSethi <panav.sethi.delhi@gmail.com>
2021-08-23 23:11:07 +0530

    build: added feed layout

---
+++ src/components/feed/Feed.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Feed() {
+    return (
+        <div className="feed">
+            <div className="feed__nav"></div>
+            <div className="feed__content">
+                <div className="feed__content__left"></div>
+                <div className="feed__content__center"></div>
+                <div className="feed__content__right"></div>
+            </div>
+        </div>
+    )
+}
+
+export default Feed
