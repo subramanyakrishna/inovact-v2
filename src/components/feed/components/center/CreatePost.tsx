087407b0ec5a4e513d3654fc0ec4e7152bb8d48b
PanavSethi <panav.sethi.delhi@gmail.com>
2021-08-25 00:03:00 +0530

    merged

---
+++ src/components/feed/components/center/CreatePost.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import avatar from '../../../../images/feed/user-avatar.png'
+import add from '../../../../images/feed/add.svg'
+
+function CreatePost() {
+    return (
+        <>
+            <div className="create-post">
+                <div className="create-post__avatar">
+                    <img src={avatar} alt="Avatar" />
+                </div>
+                <div className="create-post__content">
+                    <h1>What's on your mind, Matt?</h1>
+                    <div className="create-post__content__options">
+                        <div className="create-post__content__options__button">
+                            New Project
+                        </div>
+                        <div className="create-post__content__options__button">
+                            New Idea
+                        </div>
+                        <div className="create-post__content__options__button">
+                            New Thoughts
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="floating-button">
+                <img src={add} alt="" />
+            </div>
+        </>
+    )
+}
+
+export default CreatePost
