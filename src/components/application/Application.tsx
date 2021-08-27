c42102bfaa7d87b6c569232f8579d7c432fc992f
PanavSethi <panav.sethi.delhi@gmail.com>
2021-08-27 12:56:04 +0530

    chore: added connections, profile, teams directories

--- src/components/application/Application.tsx
+++ src/components/application/Application.tsx
@@ -1,15 +1,13 @@
 import React from 'react'
-import Navbar from 'components/application/components/NavBar'
 import Feed from 'components/application/components/feed/Feed'
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
+import NavBar from 'components/application/components/NavBar'
 
 function Application() {
     return (
-        <div className="feed">
-            <div className="feed__nav">
-                <Navbar />
-            </div>
+        <div className="application">
             <Router>
+                <NavBar />
                 <Switch>
                     <Route path="/feed" exact component={Feed} />
                 </Switch>
