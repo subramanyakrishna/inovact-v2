d4eccbf60396ea924f6190accf7cc3ed88b321bc
PanavSethi <panav.sethi.delhi@gmail.com>
2021-09-02 23:40:54 +0530

    build: post cards created and routing fixed

--- src/components/application/Application.tsx
+++ src/components/application/Application.tsx
@@ -2,6 +2,12 @@ import React from 'react'
 import Feed from 'components/application/components/feed/Feed'
 import NavBar from 'components/application/components/NavBar'
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
+import Connections from 'components/application/components/connections/Connections'
+import Profile from './components/profile/Profile'
+import Teams from './components/teams/Teams'
+import Notifications from './components/notifications/Notifications'
+import Settings from './components/settings/Settings'
+import Messages from './components/messages/Messages'
 
 function Application() {
     return (
@@ -10,6 +16,12 @@ function Application() {
                 <NavBar />
                 <Switch>
                     <Route path="/feed" exact component={Feed} />
+                    <Route path="/connections" exact component={Connections} />
+                    <Route path="/profile" exact component={Profile} />
+                    <Route path="/teams" component={Teams} />
+                    <Route path="/notifications" component={Notifications} />
+                    <Route path="/settings" component={Settings} />
+                    <Route path="/messages" component={Messages} />
                 </Switch>
             </Router>
         </div>
---
+++ src/components/application/components/feed/components/center/Photos.tsx
@@ -0,0 +1,53 @@
+import React, { useState, useCallback, useEffect } from 'react'
+import Gallery from 'react-photo-gallery'
+import Carousel, { Modal, ModalGateway } from 'react-images'
+
+type photoProps = {
+    images: string[]
+}
+
+function Photos(props: photoProps) {
+    const [currentImage, setCurrentImage] = useState(0)
+    const [viewerIsOpen, setViewerIsOpen] = useState(false)
+    const openLightbox = useCallback((event, { photo, index }) => {
+        setCurrentImage(index)
+        setViewerIsOpen(true)
+    }, [])
+
+    const closeLightbox = () => {
+        setCurrentImage(0)
+        setViewerIsOpen(false)
+    }
+
+    var photos = props.images.map((img) => {
+        return {
+            src: img,
+            width: 1,
+            height: 1,
+            srcSet: img,
+            title: '',
+        }
+    })
+
+    return (
+        <div>
+            <Gallery photos={photos} onClick={openLightbox} />
+            <ModalGateway>
+                {viewerIsOpen ? (
+                    <Modal onClose={closeLightbox}>
+                        <Carousel
+                            currentIndex={currentImage}
+                            views={photos.map((x) => ({
+                                ...x,
+                                srcset: x.srcSet,
+                                caption: x.title,
+                            }))}
+                        />
+                    </Modal>
+                ) : null}
+            </ModalGateway>
+        </div>
+    )
+}
+
+export default Photos
