c8650ccb75364b56792b605309a16e8e7d6fa91b
khushilms-cs19 <khushilms.cs19@bmsce.ac.in>
2021-10-24 23:45:33 +0530

    worked on the date issue

--- src/components/application/components/settings/components/YourProfile/components/UserEducation.tsx
+++ src/components/application/components/settings/components/YourProfile/components/UserEducation.tsx
@@ -36,12 +36,15 @@ function UserEducation({ handleUserInfoChange, userInfo }: any) {
                         <input
                             type="text"
                             placeholder="Eg .   2011"
-                            value={userInfo['graduation_year']}
-                            onChange={(e) =>
+                            defaultValue={new Date(userInfo['graduation_year']).getFullYear()}
+                            onChange={(e) =>{
+                                if(e.target.value.length<4 || e.target.value.length>4){
+                                    return;
+                                }
                                 handleUserInfoChange(
                                     'graduation-year',
-                                    e.target.value
-                                )
+                                    new Date(e.target.value).toISOString())
+                            }
                             }
                         />
                     </div>
