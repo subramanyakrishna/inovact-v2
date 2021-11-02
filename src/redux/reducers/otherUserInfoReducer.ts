2c18de1b21d14c6f620c7ed68d3d6d0e8b855ec0
subramanyakrishna <subramanyakrishna2000@gmail.com>
2021-11-02 23:20:51 +0530

    corrected error in otheruserprofile

--- src/redux/reducers/otherUserInfoReducer.ts
+++ src/redux/reducers/otherUserInfoReducer.ts
@@ -1,17 +1,40 @@
-import { otherUserInfoConstants } from "redux/actionTypes/otherUserInfoConstants";
+import { otherUserInfoConstants } from 'redux/actionTypes/otherUserInfoConstants'
 
-const initialState: any = {};
+const initialState: any = {
+    user_skills: [
+        {
+            skill: {
+                id: 1,
+                name: 'test skill',
+            },
+            level: 'beginner',
+        },
+        {
+            skill: {
+                id: 2,
+                name: 'leadership',
+            },
+            level: 'intermediate',
+        },
+        {
+            skill: {
+                id: 3,
+                name: 'communication',
+            },
+            level: 'advanced',
+        },
+    ],
+}
 
-const otherUserInfoReducer = (state= initialState, action: any)=>{
-    switch(action.type){
+const otherUserInfoReducer = (state = initialState, action: any) => {
+    switch (action.type) {
         case otherUserInfoConstants.OTHER_USER_UPDATE_INFO:
-            return action.payload;
+            return { ...state, ...action.payload }
         case otherUserInfoConstants.OTHER_USER_CLEAR_INFO:
-            return initialState;
-        default: return state;    
+            return initialState
+        default:
+            return state
     }
 }
 
-export {
-    otherUserInfoReducer
-}
+export { otherUserInfoReducer }
