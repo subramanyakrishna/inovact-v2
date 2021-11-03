2337d75c70bf7a589f4f98972bcb0bd4cd8cb07a
subramanyakrishna <subramanyakrishna2000@gmail.com>
2021-11-03 15:29:06 +0530

    connected notisfication with api

--- src/components/application/components/notifications/NotificationTag.tsx
+++ src/components/application/components/notifications/NotificationTag.tsx
@@ -1,44 +1,148 @@
-import {useState} from 'react';
-
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 function NotificationTag(props: any) {
-    const [showOptions, setShowOptions] = useState(false);
-    const toggleShowOptionsMenu = ()=>{
-        setShowOptions(!showOptions);
+    const [showOptions, setShowOptions] = useState(false)
+    const [state, setState] = useState<any>({})
+    const toggleShowOptionsMenu = () => {
+        setShowOptions(!showOptions)
     }
-    const removeOptionsSlow = ()=>{
-        setTimeout(()=>{
-            setShowOptions(false);
-        },500);
+    const removeOptionsSlow = () => {
+        setTimeout(() => {
+            setShowOptions(false)
+        }, 500)
     }
+    const mapApiNotisficationDataToUiData = (
+        notisficationFromApi: any,
+        notisfiation_type: string
+    ) => {
+        switch (notisfiation_type) {
+            case 'connectionsByUser2': {
+                return {
+                    id: notisficationFromApi.user1,
+                    img: 'yet to be added',
+                    type: 'connectionsByUser2',
+                    comment: (
+                        <p className="notifications-tag-comment">
+                            <b>{notisficationFromApi.user1}</b> want to connect
+                            with you
+                        </p>
+                    ),
+                    time: 'yes to be added',
+                }
+            }
+            case 'ideas':
+                return {
+                    id: -1,
+                    img: 'yet to be added',
+                    type: 'ideas',
+                    comment: (
+                        <p className="notifications-tag-comment">
+                            Yet to be added ideas
+                        </p>
+                    ),
+                    time: 'yes to be added',
+                }
+            case 'thoughts':
+                return {
+                    id: -1,
+                    img: 'yet to be added',
+                    type: 'thoughts',
+                    comment: (
+                        <p className="notifications-tag-comment">
+                            Yet to be added thoughts
+                        </p>
+                    ),
+                    time: 'yes to be added',
+                }
+            case 'projects': {
+                return {
+                    id: notisficationFromApi.id,
+                    img: notisficationFromApi.project_likes[0].user.avatar,
+                    type: 'projects',
+                    comment: (
+                        <p className="notifications-tag-comment">
+                            <b>
+                                {notisficationFromApi.project_likes[0].user
+                                    .first_name +
+                                    ' ' +
+                                    notisficationFromApi.project_likes[0].user
+                                        .last_name}
+                            </b>{' '}
+                            liked the project{' '}
+                            <Link to={`/posts/${notisficationFromApi.id}`}>
+                                <b>{notisficationFromApi.title}</b>
+                            </Link>
+                        </p>
+                    ),
+                    time: 'yes to be added',
+                }
+            }
+            case 'team_invitations': {
+                return {
+                    id: notisficationFromApi.team.id,
+                    img: 'yet to be added',
+                    type: 'team_invitations',
+                    comment: (
+                        <p className="notifications-tag-comment">
+                            you are invited to team{' '}
+                            <b>{notisficationFromApi.team.name}</b>
+                        </p>
+                    ),
+                    time: 'yes to be added',
+                }
+            }
+        }
+        console.log(notisfiation_type, notisficationFromApi)
+        return notisficationFromApi
+    }
+    useEffect(() => {
+        const uiMappedNotisficationData = mapApiNotisficationDataToUiData(
+            props.notisfication,
+            props.notisfication_type
+        )
+        setState(uiMappedNotisficationData)
+    }, [])
     return (
         <div className="notifications-tag">
             <div className="notifications-tag-img-container">
-                <img src={props.img} alt=""/>
+                <img src={state.img} alt="" />
             </div>
-            <p className="notifications-tag-comment">
-                <b>{props.name} </b> 
-                {props.comment}
-            </p>
-            <p className="notifications-tag-time">{props.time}</p>
+            {state.comment}
+            <p className="notifications-tag-time">{state.time}</p>
             <div>
-                <button className="notifications-tag-optionsbtn" onClick={toggleShowOptionsMenu}>&#8942;</button>
-                {
-                    showOptions && 
-                    <div className="notifications-options" onMouseLeave={removeOptionsSlow}>
+                <button
+                    className="notifications-tag-optionsbtn"
+                    onClick={() => toggleShowOptionsMenu()}
+                >
+                    &#8942;
+                </button>
+                {showOptions && (
+                    <div
+                        className="notifications-options"
+                        onMouseLeave={removeOptionsSlow}
+                    >
                         <div>
-                            <span className="notifications-options-heading">Delete</span>
-                            <span className="notifications-options-description">Delete this notification</span>
+                            <span className="notifications-options-heading">
+                                Delete
+                            </span>
+                            <span className="notifications-options-description">
+                                Delete this notification
+                            </span>
                         </div>
                         <div>
-                            <span className="notifications-options-heading">Turn off</span>
-                            <span className="notifications-options-description">Stop seeing {props.name}'s updates</span>
+                            <span className="notifications-options-heading">
+                                Turn off
+                            </span>
+                            <span className="notifications-options-description">
+                                Stop seeing {props.type}'s updates
+                            </span>
                         </div>
                     </div>
-                }
+                )}
             </div>
         </div>
     )
 }
 
-export default NotificationTag;
+export default NotificationTag
--- src/components/application/components/notifications/Notifications.tsx
+++ src/components/application/components/notifications/Notifications.tsx
@@ -1,71 +1,47 @@
-import React from 'react'
+import Spinner from 'components/application/Spinner'
+import React, { useEffect, useState } from 'react'
+import makeApiCall from '../settings/makeApiCall'
 import NotificationTag from './NotificationTag'
 
 function Notifications() {
-    const data = [
-        {
-            name: 'Girinath Nataraj',
-            image: 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
-            comment:
-                'Egestas sed sit quam mattis vitae eu egastas sed sit quam mattis sem at et.',
-            time: '1hr',
-        },
-        {
-            name: 'Girinath Nataraj',
-            image: 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
-            comment:
-                'Egestas sed sit quam mattis vitae eu egastas sed sit quam mattis sem at et.',
-            time: '1hr',
-        },
-        {
-            name: 'Girinath Nataraj',
-            image: 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
-            comment:
-                'Egestas sed sit quam mattis vitae eu egastas sed sit quam mattis sem at et.',
-            time: '1hr',
-        },
-        {
-            name: 'Girinath Nataraj',
-            image: 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
-            comment:
-                'Egestas sed sit quam mattis vitae eu egastas sed sit quam mattis sem at et.',
-            time: '1hr',
-        },
-        {
-            name: 'Girinath Nataraj',
-            image: 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
-            comment:
-                'Egestas sed sit quam mattis vitae eu egastas sed sit quam mattis sem at et.',
-            time: '1hr',
-        },
-        {
-            name: 'Girinath Nataraj',
-            image: 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
-            comment:
-                'Egestas sed sit quam mattis vitae eu egastas sed sit quam mattis sem at et.',
-            time: '1hr',
-        },
-        {
-            name: 'Girinath Nataraj',
-            image: 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
-            comment:
-                'Egestas sed sit quam mattis vitae eu egastas sed sit quam mattis sem at et.',
-            time: '1hr',
-        },
-    ]
+    const [allNotisfication, setAllNotisfication] = useState<any>({})
+    const [isLoad, setIsLoad] = useState<boolean>(true)
+    useEffect(() => {
+        ;(async () => {
+            try {
+                const res = await makeApiCall('get', 'notifications')
+                setAllNotisfication(res.data.data.user[0])
+                setIsLoad(false)
+            } catch (err) {
+                console.log(err)
+            }
+        })()
+    }, [])
     return (
         <div className="notifications-main">
-            {data.map((noti: any, i: number) => {
-                return (
-                    <NotificationTag
-                        key={i}
-                        name={noti.name}
-                        comment={noti.comment}
-                        time={noti.time}
-                        img={noti.image}
-                    />
-                )
-            })}
+            {isLoad && <Spinner />}
+            {!isLoad &&
+                [
+                    'connectionsByUser2',
+                    'ideas',
+                    'projects',
+                    'team_invitations',
+                    'thoughts',
+                ].map((notisfication_type: string) => {
+                    const eachNotification =
+                        allNotisfication[notisfication_type]
+                    if (eachNotification) {
+                        return eachNotification.map((notisfication: any) => {
+                            return (
+                                <NotificationTag
+                                    key={Math.round(Math.random() * 10000)}
+                                    notisfication={notisfication}
+                                    notisfication_type={notisfication_type}
+                                />
+                            )
+                        })
+                    }
+                })}
         </div>
     )
 }
